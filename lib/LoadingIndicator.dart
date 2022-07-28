05a30aa6b97f270ec598666e99f219015003d5fe
dhivainfo084@gmail.com <dhivainfo084@gmail.com>
2022-07-28 08:51:30 +0530

    added button and [progress bars]

---
+++ lib/LoadingIndicator.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
+
+class ProgressBar extends StatefulWidget {
+  const ProgressBar({Key? key}) : super(key: key);
+
+  @override
+  State<ProgressBar> createState() => _ProgressBarState();
+}
+
+class _ProgressBarState extends State<ProgressBar> {
+  int progressValue = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return SfRadialGauge(
+        title: GaugeTitle(
+            text: 'Speedometer',
+            textStyle:
+                const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
+        axes: <RadialAxis>[
+          RadialAxis(minimum: 0, maximum: 150, ranges: <GaugeRange>[
+            GaugeRange(
+                startValue: 0,
+                endValue: 50,
+                color: Colors.green,
+                startWidth: 10,
+                endWidth: 10),
+            GaugeRange(
+                startValue: 50,
+                endValue: 100,
+                color: Colors.orange,
+                startWidth: 10,
+                endWidth: 10),
+            GaugeRange(
+                startValue: 100,
+                endValue: 150,
+                color: Colors.red,
+                startWidth: 10,
+                endWidth: 10)
+          ], pointers: <GaugePointer>[
+            NeedlePointer(value: progressValue.roundToDouble())
+          ], annotations: <GaugeAnnotation>[
+            GaugeAnnotation(
+                widget: Container(
+                    child: const Text('90.0',
+                        style: TextStyle(
+                            fontSize: 25, fontWeight: FontWeight.bold))),
+                angle: 90,
+                positionFactor: 0.5)
+          ])
+        ]);
+  }
+
+  void setProgressValue(int value) {
+    setState(() {
+      progressValue = value;
+    });
+  }
+}
